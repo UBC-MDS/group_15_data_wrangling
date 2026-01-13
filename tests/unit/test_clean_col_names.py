73e8e12999d64ab1bd902fac8988d03073661a5a
Shihan66 <243736206@qq.com>
2026-01-13 15:07:49 -0800

    add wrong input test

--- tests/unit/test_clean_col_names.py
+++ tests/unit/test_clean_col_names.py
@@ -1,15 +1,15 @@
 """
 Docstring for tests.unit.test_clean_col_names
 """
-
+import pytest
 import pandas as pd
 from group_15_data_wrangling.clean_col_name import clean_col_name
 
-# df: test dataframe, out: dataframe preocessed by function
+# df: original test dataframe, out: dataframe after using the function
 df = pd.read_csv("data_sample.csv")
 out = clean_col_name(df)
 
-# make sure the structure keeps the same after applying the function
+# the structure keeps the same after applying the function
 assert out.shape[1] == df.shape[1]
 
 # change column name "fnlwgt" to "final_weight"
@@ -22,7 +22,7 @@ assert "capital_gain" in out.columns
 assert "capital_loss" in out.columns
 assert "hours_per_week" in out.columns
 
-# columns not need to be changed stay the same
+# columns not need to be changed should stay the same
 assert "age" in out.columns
 assert "sex" in out.columns
 assert "workclass" in out.columns
@@ -32,4 +32,5 @@ assert "fnlwgt" in df.columns
 assert "education.num" in df.columns
 
 # wrong input
-with 
+with pytest.raises(TypeError):
+    clean_col_name(["not", "dataframe"])
