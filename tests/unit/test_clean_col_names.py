c763cd094053ae1c67868879aa2009569694e63a
Shihan66 <243736206@qq.com>
2026-01-13 14:38:02 -0800

    initial test file

---
+++ tests/unit/test_clean_col_names.py
@@ -0,0 +1,6 @@
+"""
+Docstring for tests.unit.test_clean_col_names
+"""
+
+from group_15_data_wrangling.clean_col_name import clean_col_name
+# testing on a test dataframe, named resulted dataframe as "out"
